fec9e3abfafe199ecba3b4fbcb072dd82f3b7b31
Maykel Santos <eumaykelandre@gmail.com>
2023-02-01 14:44:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,31 +16,3 @@ df.reset_index(inplace = True)
 df.columns = ['Ano', 'PIB']
 df['PIB'] = df['PIB'].astype('int')
 pais = dados[0]['resultados'][0]['series'][0]['localidade']['nome']
-
-app = Dash(__name__)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
